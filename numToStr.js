b5de8d373db88ed39043afe8a82cfbd49ee27bf0
Walter <98433571+walter-m1@users.noreply.github.com>
2023-02-25 17:16:27 -0500

    new code added

---
+++ numToStr.js
@@ -0,0 +1,9 @@
+// write code to convert any number given to a string
+
+function numberToString(num) {
+    return num.toString();
+    }
+
+const numberToStr = num => `${num}`
+
+const numberToString = num => `${num}`;
